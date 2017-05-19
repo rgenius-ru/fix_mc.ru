8d716776b10d5f072e9173aede3c7badf1362199
rgenius-ru <rgenius@yandex.ru>
2017-05-19 13:33:24 +0300

    add calculate price for meters

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
 var iframeChkReadyNum = 60;//counter - number of Check iframe ready
 var iframeChkReadyCount = 0;
 var intervalID;
-var logOn = true;
+var logOn = false;
 //**************************
 console.log("Run Fix_mc Script");
 
@@ -41,7 +41,8 @@ function runScript(){
 function main() {
   var myframe = document.getElementById('tip_f');
   if (typeof myframe !== 'undefined') {
-    myframe.style.height = '230px';
+    myframe.style.height = '250px';
+    //myframe.style.width = '530px';
     
     var content1 = myframe.contentWindow.document.getElementById('background');
     //content1.insertAdjacentHTML('afterend', '<script language="Javascript" type="text/javascript">function modifyText(){this.innerHTML = "<b>Куценко</b>";}</script>');
@@ -61,30 +62,32 @@ function main() {
     var cells2 = [];
     var cells3 = [];
     var cells4 = [];
-    for(i=0; i<=5; i++){
+    for(i=0; i<=6; i++){
       cells1.push(row1.insertCell(i));
       cells2.push(row2.insertCell(i));
       cells3.push(row3.insertCell(i));
       cells4.push(row4.insertCell(i));
     }
 
-    cells1[4].innerHTML = '<b>Наценка</b>';
+    cells1[1].innerHTML = '<b>Наценка</b>';
     element1 = cells1[4];
     
     cells2[3].innerHTML = '<b>За тонну</b>';
-    cells2[4].innerHTML = '<input type="text" autofocus="autofocus" id="nacenka" name="nacenka" tabindex="1" style="width: 80; text-align: center">';
+    cells2[4].innerHTML = '<b>Сумма+Нац.</b>';
+    cells2[1].innerHTML = '<input type="text" autofocus="autofocus" id="nacenka" name="nacenka" tabindex="1" style="width: 58; text-align: center">';
 
     cells3[2].innerHTML = '<b>За мп</b>';
     //cells3[1].innerHTML = '<input type="text" autocomplete="off" id="tonns1" name="tonns1" tabindex="1" value="" style="width: 58; text-align: center">'; /*onkeyup="modifyText()"*/
     //cells3[2].innerHTML = '<input type="text" autocomplete="off" id="meters1" name="meters1" onkeyup="" tabindex="1" style="width: 58; text-align: center">';
-    cells3[3].innerHTML = '<input type="text" id="price_basket1" name="price_basket1" tabindex="1" style="width: 80; text-align: center">';
-    cells3[4].innerHTML = '<input type="text" id="amount1" name="amount1" tabindex="1" style="width: 80; text-align: center">'; /*disabled="disabled"*/
+    cells3[3].innerHTML = '<input type="text" id="price_basket1" name="price_basket1" tabindex="1" readonly="readonly" style="width: 80; text-align: center; cursor: text">';
+    cells3[4].innerHTML = '<input type="text" id="amount1" name="amount1" tabindex="1" readonly="readonly" style="width: 80; text-align: center; cursor: text">';
+    cells3[1].innerHTML = '<input type="text" id="nacenka1" name="nacenka1" tabindex="1" readonly="readonly" style="width: 58; text-align: center; cursor: text">';
     
     //cells4[1].innerHTML = '<input type="text" autocomplete="off" id="tonns2" name="tonns2" tabindex="1" value="" style="width: 58; text-align: center">'; /*onkeyup="modifyText()"*/
-    cells4[2].innerHTML = '<input type="text" autocomplete="off" id="meters2" name="meters2" onkeyup="" tabindex="1" style="width: 58; text-align: center">';
-    cells4[3].innerHTML = '<input type="text" id="price_basket2" name="price_basket2" tabindex="1" style="width: 80; text-align: center">';
-    cells4[4].innerHTML = '<input type="text" id="amount2" name="amount2" tabindex="1" style="width: 80; text-align: center">'; /*disabled="disabled"*/
-    
+    cells4[2].innerHTML = '<input type="text" autocomplete="off" id="meters2" name="meters2" onkeyup="" tabindex="1" readonly="readonly" style="width: 58; text-align: center; cursor: text">';
+    //cells4[3].innerHTML = '<input type="text" id="price_basket2" name="price_basket2" tabindex="1" disabled="disabled" style="width: 80; text-align: center; cursor: text">';
+    cells4[4].innerHTML = '<input type="text" id="amount2" name="amount2" tabindex="1" readonly="readonly" style="width: 80; text-align: center; cursor: text">';
+    cells4[1].innerHTML = '<input type="text" id="nacenka2" name="nacenka2" tabindex="1" readonly="readonly" style="width: 58; text-align: center; cursor: text">';
     
     element1 = myframe.contentWindow.document.getElementById('amount1');
     //console.log(element1);
@@ -108,10 +111,15 @@ function main() {
 function calculate(frame){
   amount = frame.contentWindow.document.getElementById('amount');
   amount1 = frame.contentWindow.document.getElementById('amount1');
+  amount2 = frame.contentWindow.document.getElementById('amount2');
   nacenka = frame.contentWindow.document.getElementById('nacenka');
+  nacenka1 = frame.contentWindow.document.getElementById('nacenka1');
+  nacenka2 = frame.contentWindow.document.getElementById('nacenka2');
   tonns = frame.contentWindow.document.getElementById('tonns');
   price_basket1 = frame.contentWindow.document.getElementById('price_basket1');
   meters1 = frame.contentWindow.document.getElementById('meters1');
+  meters2 = frame.contentWindow.document.getElementById('meters2');
+  meters = frame.contentWindow.document.getElementById('meters');
   
   if (logOn){
     console.log(parseInt(amount.value));
@@ -131,7 +139,19 @@ function calculate(frame){
     price_basket1temp = amount1temp / parseFloat(tonns.value.replace(',', '.'));
     price_basket1temp = Math.ceil(price_basket1temp/10, 1) * 10;
     price_basket1.value =( price_basket1temp ).toString();
-    amount1.value = Math.round(parseFloat(tonns.value.replace(',', '.')) * price_basket1temp).toString();   
+    amount1.value = Math.ceil(parseFloat(tonns.value.replace(',', '.')) * price_basket1temp).toString();
+    meters2.value = Math.ceil(amount1temp / parseInt(meters.value)).toString();
+    amount2.value = (parseInt(meters2.value) * parseInt(meters.value)).toString();
+    nacenka1.value = (parseInt(amount1.value) - parseInt(amount.value)).toString();
+    nacenka2.value = (parseInt(amount2.value) - parseInt(amount.value)).toString();
+  }
+  else{
+    price_basket1.value = '';
+    amount1.value = '';
+    amount2.value = '';
+    meters2.value = '';
+    nacenka1.value = '';
+    nacenka2.value = '';
   }
 }
 
